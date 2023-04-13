3ddab9b423afee67b137a6cfc1eb0ad5a4a5a694
mavix21 <marcevizcarra2109@hotmail.com>
2023-04-13 10:31:46 -0500

    Feat: Add models imports and classes variable

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,16 @@
 """This module defines the routes for app_views"""
 from flask import jsonify
 from . import app_views
+from models.amenity import Amenity
+from models.base_model import Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"amenities": Amenity, "cities": City,
+           "places": Place, "reviews": Review, "states": State, "users": User}
 
 
 @app_views.route("/status", strict_slashes=False)
